3fb2e4bbfe01eccde6d4e27ec95868d553eebb80
Po-han Li <34739111+d31003@users.noreply.github.com>
2019-06-11 01:19:55 +0800

    Add files via upload

--- HW4/main.py
+++ HW4/main.py
@@ -13,11 +13,18 @@ def parse():
     parser = argparse.ArgumentParser(description="MLDS 2018 HW4")
     parser.add_argument('--env_name', default=None, help='environment name')
     parser.add_argument('--train_pg', action='store_true', help='whether train policy gradient')
-    parser.add_argument('--train_pg_improved', action='store_true', help='whether train policy gradient')
+    parser.add_argument('--train_pg_improved', action='store_true', help='whether train PPO')
     parser.add_argument('--train_dqn', action='store_true', help='whether train DQN')
     parser.add_argument('--test_pg', action='store_true', help='whether test policy gradient')
-    parser.add_argument('--test_pg_improved', action='store_true', help='whether test policy gradient')
+    parser.add_argument('--test_pg_improved', action='store_true', help='whether test PPO')
     parser.add_argument('--test_dqn', action='store_true', help='whether test DQN')
+    parser.add_argument('--test_dqn_improved', action='store_true', help='whether test Dueling DQN')
+    parser.add_argument('--train_dqn_improved', action='store_true', help='whether train Dueling DQN')
+    parser.add_argument('--test_ddqn', action='store_true', help='whether test DDQN')
+    parser.add_argument('--train_ddqn', action='store_true', help='whether train DDQN')
+    parser.add_argument('--test_dddqn', action='store_true', help='whether test DDDQN')
+    parser.add_argument('--train_dddqn', action='store_true', help='whether train DDDQN')
+
     try:
         from argument import add_arguments
         parser = add_arguments(parser)
@@ -49,6 +56,13 @@ def run(args):
         agent = Agent_DQN(env, args)
         agent.train()
 
+    if args.train_dqn_improved:
+        env_name = args.env_name or 'BreakoutNoFrameskip-v4'
+        env = Environment(env_name, args, atari_wrapper=True)
+        from agent_dir.agent_dqn_improved import Agent_DQN
+        agent = Agent_DQN(env, args)
+        agent.train()
+
     if args.test_pg:
         env = Environment('Pong-v0', args, test=True)
         from agent_dir.agent_pg import Agent_PG
@@ -64,6 +78,38 @@ def run(args):
     if args.test_dqn:
         env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
         from agent_dir.agent_dqn import Agent_DQN
+        agent = Agent_DQN(env, args)
+        test(agent, env, total_episodes=100)
+
+    if args.test_dqn_improved:
+        env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
+        from agent_dir.agent_dqn_improved import Agent_DQN
+        agent = Agent_DQN(env, args)
+        test(agent, env, total_episodes=100)
+
+    if args.train_ddqn:
+        env_name = args.env_name or 'BreakoutNoFrameskip-v4'
+        env = Environment(env_name, args, atari_wrapper=True)
+        from agent_dir.agent_ddqn import Agent_DQN
+        agent = Agent_DQN(env, args)
+        agent.train()
+    
+    if args.test_ddqn:
+        env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
+        from agent_dir.agent_ddqn import Agent_DQN
+        agent = Agent_DQN(env, args)
+        test(agent, env, total_episodes=100)
+
+    if args.train_dddqn:
+        env_name = args.env_name or 'BreakoutNoFrameskip-v4'
+        env = Environment(env_name, args, atari_wrapper=True)
+        from agent_dir.agent_dddqn import Agent_DQN
+        agent = Agent_DQN(env, args)
+        agent.train()
+    
+    if args.test_dddqn:
+        env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
+        from agent_dir.agent_dddqn import Agent_DQN
         agent = Agent_DQN(env, args)
         test(agent, env, total_episodes=100)
 
